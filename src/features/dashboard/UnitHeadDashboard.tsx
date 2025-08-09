3c603cb8caa7851688323d052d8f8b50c38c1240
= <prince.muteh@gmail.com>
2025-08-09 16:00:14 +0100

    updates

--- src/features/dashboard/UnitHeadDashboard.tsx
+++ src/features/dashboard/UnitHeadDashboard.tsx
@@ -168,6 +168,7 @@ export default function UnitHeadDashboard({ role }: { role: string }) {
                                     <AddLawyerSheet />
                                 </>
                             )}
+                            
                         </div>
                     </div>
 
